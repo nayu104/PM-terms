afb7332ed1644e7bc2113fba9aa0259e016efff5
nayu <ose794@gamil.com>
2025-11-09 20:27:23 +0900

    add: 利用規約のテキストをApp.tsxに書き込んだ

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const sections =[
+  {
+    title: '利用規約',
+    content: '最終更新日: 2025年1月1日'
+  },
+  {
+    title: '第1条（適用範囲）',
+    content: 'この利用規約は、〇〇が提供するサービス（以下「本サービス」といいます。）の利用条件を定めるものです。'
+  },
+  {
+    title: '第2条（利用規約の変更）',
+    content: '当社は、必要に応じて、本利用規約を変更することがあります。変更後の利用規約は、本サービスの公式サイトに掲載された時点で有効となります。'
+  },
+  {
+    title: '第3条（利用規約の遵守）',
+    content: '利用者は、本利用規約を遵守し、本サービスを利用することを承諾します。'
+  },
+]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <section>
+      <h2>{sections[0].title}</h2>
+      <p>{sections[0].content}</p>
+     </section>
+     <section>
+      <h2>{sections[1].title}</h2>
+      <p>{sections[1].content}</p>
+     </section>
+     <section>
+      <h2>{sections[2].title}</h2>
+      <p>{sections[2].content}</p>
+     </section>
+     <section>
+      <h2>{sections[3].title}</h2>
+      <p>{sections[3].content}</p>
+     </section>
+     </>
   )
 }
 
